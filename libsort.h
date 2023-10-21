e38aade7783b94622ebd670af768d51f6e6bb93c
Michael Gourion <michagourion@gmail.com>
2023-10-21 22:25:28 +0300

    headers

---
+++ libsort.h
@@ -0,0 +1,12 @@
+#ifndef LIBSORT_H
+#define LIBSORT_H
+
+void swap(int *a, int *b);
+
+void printArray(int array[], int size);
+
+void bubbleSort(int array[], int size);
+
+void selectionSort(int array[], int size);
+
+#endif
